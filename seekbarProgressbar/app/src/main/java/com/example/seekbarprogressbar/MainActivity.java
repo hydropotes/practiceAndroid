85be9362795477d3178accecdbd7d1b7b8d0cb8f
rudwear <rudwear@gmail.com>
2019-05-04 11:07:40 +0900

    practice seek_progressbar

--- seekbarProgressbar/app/src/main/java/com/example/seekbarprogressbar/MainActivity.java
+++ seekbarProgressbar/app/src/main/java/com/example/seekbarprogressbar/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.seekbarprogressbar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,27 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SeekBar seekBar1 = findViewById(R.id.seekBar1);
+        final ProgressBar progressBar1 = findViewById(R.id.progressBar1);
+        final EditText editText1 = findViewById(R.id.editText1);
+
+        seekBar1.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                progressBar1.setProgress(progress);
+                editText1.setText(Integer.toString(progress));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
     }
 }
